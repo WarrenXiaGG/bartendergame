b7d54ab129c303169363a973a83668e7448e74a7
warren <rlaterson@gmail.com>
2018-10-24 15:14:55 -0400

    boilerplate

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
-public class Main {
-    
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    public void start(Stage stage){
+        stage.setTitle("Bartender Game");
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
